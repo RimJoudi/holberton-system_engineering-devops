982a6cc8abaacc3fad336d6b438992736907b7f3
RimJoudi <joudirim93@gmail.com>
2021-09-13 21:43:10 +0100

    commit to tasks 0, 1

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'user_id': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
+
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
+
+    done_tasks = []
+    total_tasks = len(todo_dict_list)
+    employee = user_dict_list[0].get('name')
+
+    for task in todo_dict_list:
+        if task.get('completed') is True:
+            done_tasks.append(task)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee, len(done_tasks), total_tasks))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+
+"""
+import requests
+import sys
+import csv
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
+    url_user = 'https://jsonplaceholder.typicode.com/users/'
+    todo = requests.get(url_todo, params={'userId': employee_id})
+    user = requests.get(url_user, params={'id': employee_id})
+
+    todo_dict_list = todo.json()
+    user_dict_list = user.json()
+
+    employee = user_dict_list[0].get('username')
+
+    with open("{}.csv".format(employee_id), "a+") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo_dict_list:
+            status = task['completed']
+            title = task['title']
+            writer.writerow(["{}".format(employee_id),
+                                "{}".format(employee),
+                                "{}".format(status),
+                                "{}".format(title)])
