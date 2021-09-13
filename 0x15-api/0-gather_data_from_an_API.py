fcab022885a1210fdc0c9998666f5ba46b4b1688
RimJoudi <joudirim93@gmail.com>
2021-09-13 21:53:39 +0100

    fix pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- using this REST API, for a given employee ID, 
+ using this REST API, for a given employee ID,
  returns information about his/her TODO list progress.
 """
 import requests
