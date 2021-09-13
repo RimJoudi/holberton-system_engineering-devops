30ab0a7cc4af4c45dd5d685839854e6431f4266b
RimJoudi <joudirim93@gmail.com>
2021-09-13 21:58:22 +0100

    fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-
+export data in the CSV format
 """
 import requests
 import sys
@@ -18,12 +18,12 @@ if __name__ == '__main__':
 
     employee = user_dict_list[0].get('username')
 
-    with open("{}.csv".format(employee_id), "a+") as file:
+    with open("{}.csv".format(employee_id), "w") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in todo_dict_list:
             status = task['completed']
             title = task['title']
             writer.writerow(["{}".format(employee_id),
-                                "{}".format(employee),
-                                "{}".format(status),
-                                "{}".format(title)])
+                             "{}".format(employee),
+                             "{}".format(status),
+                             "{}".format(title)])
