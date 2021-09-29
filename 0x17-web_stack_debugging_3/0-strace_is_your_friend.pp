6e866e67b13188c18e288e820a0dbf63fa82afa3
RimJoudi <joudirim93@gmail.com>
2021-09-29 20:52:51 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix the 500 Apache error using puppet
 exec { 'fix-typo':
-    command => "sed -i 's/ .phpp/ .php/' /var/www/html/wp-settings.php",
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
